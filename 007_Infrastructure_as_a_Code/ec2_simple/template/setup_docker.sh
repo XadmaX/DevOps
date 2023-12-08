9f6122f8ba2ea3c1e6c80e4b853b95237587faa4
Maksym Khalin <xadmax@gmail.com>
2023-12-08 20:59:22 +0100

    Docker post installation steps added to bash script for terraform 007 lesson

--- 007_Infrastructure_as_a_Code/ec2_simple/template/setup_docker.sh
+++ 007_Infrastructure_as_a_Code/ec2_simple/template/setup_docker.sh
@@ -15,4 +15,7 @@ sudo apt-get update -y
 
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 
+sudo usermod -aG docker $USER
+newgrp docker
+
 sudo apt-get install git -y
